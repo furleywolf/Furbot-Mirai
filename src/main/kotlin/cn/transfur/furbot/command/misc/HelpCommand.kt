a22684442a7c4fff65e9d7c23585123efe5508fa
Peanuuutz <1415605913@qq.com>
2022-02-20 12:24:57 +0800

    Simplify

--- src/main/kotlin/cn/transfur/furbot/command/misc/HelpCommand.kt
+++ src/main/kotlin/cn/transfur/furbot/command/misc/HelpCommand.kt
@@ -3,8 +3,6 @@ package cn.transfur.furbot.command.misc
 import cn.transfur.furbot.command.FurbotSimpleCommand
 import net.mamoe.mirai.console.command.CommandSenderOnMessage
 import net.mamoe.mirai.contact.Contact
-import net.mamoe.mirai.message.data.Face
-import net.mamoe.mirai.message.data.buildMessageChain
 
 object HelpCommand : FurbotSimpleCommand("帮助") {
     override val description: String = "Get command help"
@@ -13,23 +11,9 @@ object HelpCommand : FurbotSimpleCommand("帮助") {
     suspend fun CommandSenderOnMessage<*>.run() = differContact { respond(it) }
 
     private suspend fun respond(target: Contact) {
-        val message = buildMessageChain(5) {
-            // Title
-            add("--- 每日吸毛 Bot ---\n")
-            add(Face(Face.JU_HUA))
-            add("快乐吸毛每一天")
-            add(Face(Face.JU_HUA))
-            add("\n")
-
-            // Instructions link
-            add("--- 命令说明 ---\n")
-
-            // Note: there are several differences between the original commands
-            // and these in Furbot-Mirai, like session support. For now just use
-            // the original instructions.
-            add("https://www.kancloud.cn/furleywolf/furbot/2482928")
-        }
-
-        target.sendMessage(message)
+        // Note: there are several differences between the original commands
+        // and these in Furbot-Mirai, like session support. For now just use
+        // the original instructions.
+        target.sendMessage("命令说明：https://www.kancloud.cn/furleywolf/furbot/2482928")
     }
 }
